b43b06dc23e67f9bbc2b62d8683b89698b703bc6
Adrien-M-design <61044812+Adrien-M-design@users.noreply.github.com>
2021-03-03 10:09:36 +0100

    Script LabyrinthController

---
+++ DontThink/Assets/Scripts/LabyrinthController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LabyrinthController : MonoBehaviour
+{
+    [SerializeField] List<GameObject> _paterns = new List<GameObject>();
+
+    private GameObject _currentPatern = null;
+
+    private void Start()
+    {
+        _currentPatern = _paterns[Random.Range(0, _paterns.Count)];
+        Debug.Log(_currentPatern);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
