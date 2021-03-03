612e728966f8394231c4bd868913517a5dcff182
Jeremy0307 <jeremyderive@gmail.com>
2021-03-03 10:46:50 +0100

    PaternsRandom

--- DontThink/Assets/Scripts/LabyrinthController.cs
+++ DontThink/Assets/Scripts/LabyrinthController.cs
@@ -4,18 +4,31 @@ using UnityEngine;
 
 public class LabyrinthController : MonoBehaviour
 {
-    [SerializeField] List<GameObject> _paterns = new List<GameObject>();
+    [SerializeField] private List<GameObject> _paterns = new List<GameObject>();
 
-    private GameObject _currentPatern = null;
+    [SerializeField] private GameObject _currentPatern = null;
 
     private void Start()
     {
+
+        //Random Prefab
         _currentPatern = _paterns[Random.Range(0, _paterns.Count)];
         Debug.Log(_currentPatern);
+
+        //Instantie le prefab
+        Instantiate(_currentPatern, new Vector3(1, 1, -1), Quaternion.identity);
+
+        //Desactive les autres paterns
+        for (int i = 0; i < _paterns.Count; i++)
+        {
+            _paterns[i].SetActive(false);     
+                
+        }
+
     }
 
     private void Update()
     {
-        
+
     }
 }
