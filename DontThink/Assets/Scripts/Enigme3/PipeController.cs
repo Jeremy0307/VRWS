d5b0da892a0d6cf52466f79f22a4ff46f97ef8b3
Adrien-M-design <61044812+Adrien-M-design@users.noreply.github.com>
2021-03-08 15:38:22 +0100

    start enigme 3

---
+++ DontThink/Assets/Scripts/Enigme3/PipeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _pipes = null;
+    [SerializeField] private GameObject[] _snapZones = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
