7f4b3a503370b21e517dd9f134c6c34fbab39662
Jeremy0307 <jeremyderive@gmail.com>
2021-03-11 09:30:25 +0100

    AudioController

---
+++ DontThink/Assets/Scripts/Audio/AudioController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] _clips = null;
+
+    private int _clipIndex = 0;
+
+    private AudioSource _audio = null;
+
+    private bool _audioPlaying = false;
+
+    private void Start()
+    {
+        _audio = gameObject.GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!_audio.isPlaying)
+        {
+            _clipIndex = Random.Range(0, _clips.Length - 1);
+            _audio.clip = _clips[_clipIndex];
+            _audio.PlayDelayed(Random.Range(20f, 50f));
+        }
+    }
+}
