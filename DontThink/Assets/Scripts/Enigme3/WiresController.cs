0f2d7e9313834c130ce6ee7e5b359793f83832eb
Jeremy0307 <jeremyderive@gmail.com>
2021-03-12 16:16:16 +0100

    Enigme3

--- DontThink/Assets/Scripts/Enigme3/WiresController.cs
+++ DontThink/Assets/Scripts/Enigme3/WiresController.cs
@@ -8,15 +8,6 @@ public class WiresController : MonoBehaviour
 
     [SerializeField] private GameObject[] _lights = null;
 
-    private void Awake()
-    {
-        for (int i = 0; i < _lights.Length; i++)
-        {
-            _lights[i].SetActive(false);
-        }
-    }
-
-
     public void Reset()
     {
 
@@ -25,14 +16,14 @@ public class WiresController : MonoBehaviour
     public void OnPress0(Hand hand)
     {
         _lights[0].SetActive(true);
-        Reset();
+        //Reset();
 
     }
 
     public void OnPress1(Hand hand)
     {
-        _lights[0].SetActive(true);
-        Reset();
+        _lights[1].SetActive(true);
+        //Reset();
 
     }
 
