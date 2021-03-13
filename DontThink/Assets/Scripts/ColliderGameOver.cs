be112cbb8a0590d7bb7c5d94b484f076176c8b53
Jeremy0307 <jeremyderive@gmail.com>
2021-03-13 08:25:04 +0100

    WaterTime

---
+++ DontThink/Assets/Scripts/ColliderGameOver.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderGameOver : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Water"))
+        {
+            Debug.Log("GameOver");
+        }
+    }
+}
